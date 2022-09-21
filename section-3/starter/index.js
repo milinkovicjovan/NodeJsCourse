11cc04da6a4d0863da0fd8f028ed3f1038ff360d
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-21 20:22:03 +0200

    waitingForMultiplePromisesSimultaneously

--- section-3/starter/index.js
+++ section-3/starter/index.js
@@ -24,12 +24,20 @@ const getDogPic = async () => {
     const data = await readFilePromise(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const res1Promise = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
+    const res2Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const res3Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([res1Promise, res2Promise, res3Promise]);
+    const imgs = all.map((el) => el.body.message);
+    console.log(imgs);
 
-    await writeFilePromise('dog-img.txt', res.body.message);
+    await writeFilePromise('dog-img.txt', imgs.join('\n'));
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
