3e19ee0347885f376051cb414708db93787b1873
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-22 22:03:06 +0200

    chainingMultipleMiddlewearFunctions

--- section-4/starter/controllers/tourController.js
+++ section-4/starter/controllers/tourController.js
@@ -17,6 +17,18 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+  // is everything is allright we want to move on
+  // to the next middleware
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
