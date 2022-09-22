038ba2521f9079e2e9fdf59bdcf2a820266a7f4e
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-22 20:55:53 +0200

    creatingAndMountingMultipleRouters

--- section-4/starter/app.js
+++ section-4/starter/app.js
@@ -145,21 +145,20 @@ const deleteUser = (req, res) => {
 };
 
 // CHAINED ROUTES
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+// USERS ROUTES
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+// mounting the router
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
 // START SERVER
 const port = 3000;
 app.listen(port, () => {
