81775903485370e4585a838a1f7bb3d777392554
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-23 23:34:23 +0200

    anotherWayOfCreatingDocuments

--- section-4/starter/app.js
+++ section-4/starter/app.js
@@ -19,6 +19,8 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
+  // is everything is allright we want to move on
+  // to the next middleware
 });
 
 app.use((req, res, next) => {
--- section-4/starter/controllers/tourController.js
+++ section-4/starter/controllers/tourController.js
@@ -2,18 +2,6 @@ const Tour = require('./../models/tourModel');
 
 // ROUTE HANDLERS
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-  // is everything is allright we want to move on
-  // to the next middleware
-};
-
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -40,13 +28,25 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  }); // created status(201)
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({})
+    // newTour.save()
+
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    }); // created status(201)
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
