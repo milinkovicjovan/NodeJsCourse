5889b31c79c5588d8e51584cf1798b82c041c77e
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-25 19:50:40 +0200

    makingTheAPIBetterAdvancedFiltering

--- section-4/starter/controllers/tourController.js
+++ section-4/starter/controllers/tourController.js
@@ -5,22 +5,32 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // console.log(req.query, queryObj);
+    console.log(req.query, queryObj);
+    // 2) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // \b exactly gte, gt, lte... /g multiple words
+    console.log(JSON.parse(queryStr));
 
-    const query = await Tour.find(queryObj);
+    // { difficulty: 'easy', duration:{ $gte:5 } }
+    // { difficulty: 'easy', duration:{ gte:5 } }
+    // gte, gt, lte, lt
+    // gte === greater than or equal
+
+    const query = await Tour.find(JSON.parse(queryStr));
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const query = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     // SEND RESPONSE
     res.status(200).json({
