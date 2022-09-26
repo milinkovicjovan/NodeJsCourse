ca9e54a83ebff501a1bc258c15275b017bf27481
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-26 20:01:32 +0200

    queryMiddleware

--- section-4/starter/models/tourModel.js
+++ section-4/starter/models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -81,6 +85,25 @@ tourSchema.pre('save', function (next) {
 //   console.log(doc);
 //   next();
 // });
+
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  // all strings that starts with find
+  this.find({
+    secretTour: {
+      $ne: true,
+    },
+  });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  // console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
