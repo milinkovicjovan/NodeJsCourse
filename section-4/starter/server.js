4b952616ecaeb9f649e6b6fbb4ed3be9dad3d425
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-28 00:57:59 +0200

    catchingUncaughtExpections

--- section-4/starter/server.js
+++ section-4/starter/server.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION ! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env',
 });
@@ -27,9 +34,10 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLER REJECTION ! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
 });
+// SAFETY NET
