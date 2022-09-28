65957fdfbfadaf50f18ba7eae7ffeafcc93e285f
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-28 21:43:56 +0200

    protectingTourRoutesPart-2

--- section-4/starter/models/userModel.js
+++ section-4/starter/models/userModel.js
@@ -34,6 +34,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 // pre-save middleware runs between getting the data and saving it to the database
@@ -54,6 +55,19 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    // console.log(changedTimestamp, JWTTimestamp);
+    return JWTTimestamp < changedTimestamp; // 300 < 200 means false
+  }
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
