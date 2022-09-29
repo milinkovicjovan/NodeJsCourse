2a4735b66079d77904fc9329f353c4e26bc55946
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-09-30 00:12:46 +0200

    ImplementingRateLimiting

--- section-4/starter/app.js
+++ section-4/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +9,19 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 // this is middleware (function that can modify incoming request data )
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: 'Too many requests from this IP, please try again in an hour !',
+});
+
+app.use('/api', limiter);
 
 // get access to the request body on the request object
 app.use(express.json());
