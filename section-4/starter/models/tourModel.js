63439d1c0f219a4a7371a9ca9627d61c1dc60e3e
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-10-02 21:31:52 +0200

    populatingTourGuides

--- section-4/starter/models/tourModel.js
+++ section-4/starter/models/tourModel.js
@@ -155,6 +155,14 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  }); // we fill it up with actual data using populate
+
+  next();
+});
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
