e9e0fcaf7e5083a36288645916db7ecaabb96e40
milinkovicjovan <milinkovicjovanbusiness@gmail.com>
2022-10-02 22:11:48 +0200

    modellingReviewsParentReferencing

---
+++ section-4/starter/models/reviewModel.js
@@ -0,0 +1,43 @@
+// review / rating / createdAt / ref to tour / ref to user
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    ratting: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+  //  all this does is to really make sure
+  //  that when we have a virtual property,
+  //  basically a field that is not stored in the database
+  //  but calculated using some other value.
+  //  So we want this to also show up whenever there is an output.
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
